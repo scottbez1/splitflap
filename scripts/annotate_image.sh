d0255d197249a5c4f680540dcde5e41b52966e49
Scott Bezek <scottbez1@gmail.com>
2016-03-22 02:00:59 -0700

    Use RobotoMono for annotated images

--- scripts/annotate_image.sh
+++ scripts/annotate_image.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-set -ev
+set -e
+
+FONT="3d/roboto/RobotoMono-Bold.ttf"
+
+# Make sure font exists
+ls "$FONT"
+
+set -v
 
 LABEL="`date --rfc-3339=seconds`\n`git rev-parse --short HEAD`"
-convert -background black -fill white -pointsize 12 -font Ubuntu-Mono-Bold label:"$LABEL" -bordercolor black -border 3 $1 +swap -append $1
+convert -background black -fill white -pointsize 12 -font "$FONT" label:"$LABEL" -bordercolor black -border 3 $1 +swap -append $1
 
