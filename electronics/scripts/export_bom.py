c34fb338348df8176b599cab5999ef3c48d1aa1c
Scott Bezek <scottbez1@gmail.com>
2016-04-09 16:29:33 -0700

    Factor out general Xvfb and helper functions to export_util file, rewrite export_bom.sh in python

--- electronics/scripts/export_bom.py
+++ electronics/scripts/export_bom.py
@@ -38,68 +38,37 @@ from export_util import (
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
-def eeschema_plot_schematic(output_directory):
+def eeschema_export_bom(output_directory):
     wait_for_window('eeschema', '\[')
 
     logger.info('Focus main eeschema window')
     xdotool(['search', '--name', '\[', 'windowfocus'])
 
-    logger.info('Open File->Plot->Plot')
-    xdotool(['key', 'alt+f'])
-    xdotool(['key', 'p'])
-    xdotool(['key', 'p'])
+    logger.info('Open Tools->Generate Bill Of Materials')
+    xdotool(['key', 'alt+t'])
+    xdotool(['key', 'm'])
 
-    wait_for_window('plot', 'Plot')
-    xdotool(['search', '--name', 'Plot', 'windowfocus'])
-
-    logger.info('Enter build output directory')
-    xdotool(['type', output_directory])
-
-    logger.info('Select PDF plot format')
-    xdotool([
-        'key',
-        'Tab',
-        'Tab',
-        'Tab',
-        'Tab',
-        'Tab',
-        'Up',
-        'Up',
-        'Up',
-        'space',
-    ])
-
-    logger.info('Plot')
+    logger.info('Run generate')
+    wait_for_window('plot', 'Bill of Material')
+    xdotool(['search', '--name', 'Bill of Material', 'windowfocus'])
     xdotool(['key', 'Return'])
 
     logger.info('Wait before shutdown')
     time.sleep(2)
 
-def export_schematic():
+def export_bom():
     schematic_file = os.path.join(electronics_root, 'splitflap.sch')
     output_dir = os.path.join(electronics_root, 'build')
     file_util.mkdir_p(output_dir)
 
-    screencast_output_file = os.path.join(output_dir, 'export_schematic_screencast.ogv')
-    schematic_output_pdf_file = os.path.join(output_dir, 'splitflap.pdf')
-    schematic_output_png_file = os.path.join(output_dir, 'schematic.png')
+    screencast_output_file = os.path.join(output_dir, 'export_bom_screencast.ogv')
 
     with versioned_schematic(schematic_file):
         with recorded_xvfb(screencast_output_file, width=800, height=600, colordepth=24):
             with PopenContext(['eeschema', schematic_file], close_fds=True) as eeschema_proc:
-                eeschema_plot_schematic(output_dir)
+                eeschema_export_bom(output_dir)
                 eeschema_proc.terminate()
 
-    logger.info('Rasterize')
-    subprocess.check_call([
-        'convert',
-        '-density', '96',
-        schematic_output_pdf_file,
-       '-background', 'white',
-       '-alpha', 'remove',
-       schematic_output_png_file,
-   ])
-    
 if __name__ == '__main__':
-    export_schematic()
+    export_bom()
 
