f1d1edbba32aee4835e0c66f7c48eed6c3641932
Scott Bezek <scottbez1@gmail.com>
2016-12-08 23:39:13 -0800

    Try to make imagemagick label work again by hardcoding a size

--- scripts/annotate_image.sh
+++ scripts/annotate_image.sh
@@ -10,5 +10,5 @@ ls "$FONT"
 set -v
 
 LABEL="`date --rfc-3339=seconds`\n`git rev-parse --short HEAD`"
-convert -background black -fill white -pointsize 12 -font "$FONT" label:"$LABEL" -bordercolor black -border 3 $1 +swap -append $1
+convert -background black -fill white -pointsize 12 -font "$FONT" -size 300x36 label:"$LABEL" -bordercolor black -border 3 $1 +swap -append $1
 
