cae5d24054846d946ed5a15acd795cff09706c74
Scott Bezek <scottbez1@gmail.com>
2020-06-22 01:10:53 -0700

    mkdir

--- electronics/scripts/dependencies.sh
+++ electronics/scripts/dependencies.sh
@@ -4,6 +4,7 @@ set -ev
 sudo add-apt-repository --yes ppa:js-reynaud/kicad-5
 sudo apt-get update -qq
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y kicad inkscape poppler-utils xdotool recordmydesktop
+mkdir -p ~/.config/kicad
 cp /usr/share/kicad/template/fp-lib-table ~/.config/kicad/
 cp /usr/share/kicad/template/sym-lib-table ~/.config/kicad/
 
