f8639e646ef8f4a83777fb7694e5ebc20c959edb
Scott Bezek <scottbez1@gmail.com>
2020-06-22 00:12:35 -0700

    Use kicad 5

--- electronics/scripts/dependencies.sh
+++ electronics/scripts/dependencies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ev
 
-sudo add-apt-repository --yes ppa:js-reynaud/kicad-4
+sudo add-apt-repository --yes ppa:js-reynaud/kicad-5
 sudo apt-get update -qq
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y kicad inkscape poppler-utils xdotool recordmydesktop
 
