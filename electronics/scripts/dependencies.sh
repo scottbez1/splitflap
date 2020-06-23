dcde87c5909c5f46d564729657b1ab7bb18efb55
Scott Bezek <scottbez1@gmail.com>
2020-06-22 23:32:41 -0700

    Append to pcbnew config

--- electronics/scripts/dependencies.sh
+++ electronics/scripts/dependencies.sh
@@ -10,10 +10,11 @@ sudo python3 -m pip install psutil
 mkdir -p ~/.config/kicad
 cp /usr/share/kicad/template/fp-lib-table ~/.config/kicad/
 cp /usr/share/kicad/template/sym-lib-table ~/.config/kicad/
-echo "canvas_type=1" > ~/.config/kicad/pcbnew
-echo "SMaskColor_Red=0.1" > ~/.config/kicad/pcbnew
-echo "SMaskColor_Green=0.1" > ~/.config/kicad/pcbnew
-echo "SMaskColor_Blue=0.1" > ~/.config/kicad/pcbnew
-echo "RenderEngine=1" > ~/.config/kicad/pcbnew
-echo "Render_RAY_ProceduralTextures=1" > ~/.config/kicad/pcbnew
+
+echo "canvas_type=1" >> ~/.config/kicad/pcbnew
+echo "SMaskColor_Red=0.1" >> ~/.config/kicad/pcbnew
+echo "SMaskColor_Green=0.1" >> ~/.config/kicad/pcbnew
+echo "SMaskColor_Blue=0.1" >> ~/.config/kicad/pcbnew
+echo "RenderEngine=1" >> ~/.config/kicad/pcbnew
+echo "Render_RAY_ProceduralTextures=1" >> ~/.config/kicad/pcbnew
 
