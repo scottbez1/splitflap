d0a787237a033109c60cd334d81ac1764fe8f39c
Scott Bezek <scottbez1@gmail.com>
2021-03-07 21:11:04 -0800

    Don't use outdated openscad ppa

--- 3d/scripts/dependencies_legacy.sh
+++ 3d/scripts/dependencies_legacy.sh
@@ -6,6 +6,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 set -v
 
+sudo add-apt-repository --yes ppa:openscad/releases
 sudo apt-get update -qq
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y openscad inkscape imagemagick xvfb
 sudo apt-get purge -y fglrx
