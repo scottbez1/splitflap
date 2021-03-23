2d2351e106e2dcca130bb2c08dc512db3a5b62df
Dave Madison <dmadison@users.noreply.github.com>
2021-03-23 07:36:23 -0400

    Software: Run flap test starting at 1
    This way the display doesn't refresh the home flap if FORCE_FULL_ROTATION is set to 'true', and it will end on the home flap ready for another run.

--- software/flap_test.py
+++ software/flap_test.py
@@ -10,15 +10,31 @@ def run():
     print('Starting...')
     with splitflap(port) as s:
         characters = s.get_character_list()
+        num_flaps = len(characters)
 
+        # make sure all modules are home'd before we continue
         while(True):
-            for i, c in enumerate(characters):
-                string = c * s.get_num_modules()
-                print('Going to index {} (\'{}\')'.format(i, c))
-                status = s.set_text(string)
-                s.print_status(status)
-                sleep(1)
-            sleep(10)
+            at_home = True
+            for module in s.get_status():
+                at_home &= (module["flap"] == characters[0])
+            if not at_home:
+                s.recalibrate_all()
+            else:
+                break
+
+        index = 1  # we're at home (#0), start at flap #1
+
+        # loop through all flaps on every module
+        while(True):
+            char = characters[index]
+            string = char * s.get_num_modules()
+            print('Going to index {} (\'{}\')'.format(index, char))
+            status = s.set_text(string)
+            s.print_status(status)
+            if index == 0:
+                break  # we're back home
+            index = (index + 1) % num_flaps
+            sleep(1)
 
 
 if __name__ == '__main__':
