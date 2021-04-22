83e5073818917d95238ef9c3ee4ed2e10340f344
David Madison <dmadison@users.noreply.github.com>
2021-04-22 18:33:30 -0400

    Util: Catch errors in rev_info.py
    Will return "notfound" as the commit hash in cases where git is unavailable or the invoked script is not part of a git repository.

--- util/rev_info.py
+++ util/rev_info.py
@@ -2,12 +2,15 @@ import datetime
 import subprocess
 
 def git_short_rev():
-    return subprocess.check_output([
-        'git',
-        'rev-parse',
-        '--short',
-        'HEAD',
-    ]).decode('utf-8').strip()
+    try:
+        return subprocess.check_output([
+            'git',
+            'rev-parse',
+            '--short',
+            'HEAD',
+        ]).decode('utf-8').strip()
+    except (FileNotFoundError, subprocess.CalledProcessError):
+        return "notfound"
 
 def current_date():
     return datetime.date.today().strftime('%Y-%m-%d')
