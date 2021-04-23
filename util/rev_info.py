867e7219bf11104b9d059fb22f59924fcaf73b3f
David Madison <dmadison@users.noreply.github.com>
2021-04-23 02:20:04 -0400

    Revert rev_info error catching
    This reverts commits 2d011cc859013e9f05afccb8f9cb2cbc75ddb747 and 83e5073818917d95238ef9c3ee4ed2e10340f344.

--- util/rev_info.py
+++ util/rev_info.py
@@ -2,16 +2,12 @@ import datetime
 import subprocess
 
 def git_short_rev():
-    try:
-        return subprocess.check_output([
-            'git',
-            'rev-parse',
-            '--short',
-            'HEAD',
-        ]).decode('utf-8').strip()
-    except Exception as e:
-        print("Exception: " + type(e).__name__ + " while trying to find git revision")
-        return "notfound"
+    return subprocess.check_output([
+        'git',
+        'rev-parse',
+        '--short',
+        'HEAD',
+    ]).decode('utf-8').strip()
 
 def current_date():
     return datetime.date.today().strftime('%Y-%m-%d')
