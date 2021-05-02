a26f92e14fc064b8b1c96db04f7e15771936a6bb
Yifan Gu <gyf304@gmail.com>
2021-05-02 15:02:13 -0400

    Allow generation for double-sided flaps

--- 3d/scripts/generate_fonts.py
+++ 3d/scripts/generate_fonts.py
@@ -46,9 +46,14 @@ if __name__ == '__main__':
     parser.add_argument('-x', '--offset-x', type=float, help='Character offset from center, X-axis')
     parser.add_argument('-y', '--offset-y', type=float, help='Character offset from center, Y-axis')
 
+    parser.add_argument('--start-row', type=int, help="The starting row to render, 0-indexed")
+    parser.add_argument('--row-count', type=int, help="Number of rows to render")
+
+    parser.add_argument('--double-sided', action='store_true', help='Generate double sided print')
     parser.add_argument('--ncolumns', type=int, help='Number of columns / characters per row')
     parser.add_argument('--no-comp', action='store_true', default=False, help='Don\'t compensate for the gap between top and bottom flaps')
 
+    parser.add_argument('--bleed', action='store_true', help='Bleed the letters')
     parser.add_argument('--kerf', type=float, help='Override kerf_width value')
     parser.add_argument('--fill', action='store_true', help='Fill the text solid (disables optimization)')
     parser.add_argument('--skip-optimize', action='store_true', help='Don\'t remove redundant/overlapping cut lines')
@@ -78,26 +83,43 @@ if __name__ == '__main__':
     if args.offset_y is not None:
         extra_variables['letter_offset_y'] = args.offset_y
 
+    if args.start_row is not None:
+        extra_variables['start_row'] = args.start_row
+    if args.row_count is not None:
+        extra_variables['row_count'] = args.row_count
+
     if args.ncolumns is not None:
         extra_variables['num_columns'] = args.ncolumns
 
     if args.kerf is not None:
         extra_variables['kerf_width'] = args.kerf
-    if args.fill is True:
+    if args.fill:
         extra_variables['render_fill'] = True
         args.skip_optimize = True
+    if args.bleed:
+        extra_variables['bleed'] = True
 
-    renderer = Renderer(os.path.join(source_parts_dir, 'font_generator.scad'), output_directory, extra_variables)
-    renderer.clean()
-    svg_output = renderer.render_svgs(panelize_quantity = 1)
+    def render():
+        renderer = Renderer(os.path.join(source_parts_dir, 'font_generator.scad'), output_directory, extra_variables)
+        renderer.clean()
+        svg_output = renderer.render_svgs(panelize_quantity = 1)
 
-    processor = SvgProcessor(svg_output)
+        processor = SvgProcessor(svg_output)
 
-    redundant_lines, merged_lines = None, None
-    if not args.skip_optimize:
-        logging.info('Removing redundant lines')
-        redundant_lines, merged_lines = processor.remove_redundant_lines()
+        redundant_lines, merged_lines = None, None
+        if not args.skip_optimize:
+            logging.info('Removing redundant lines')
+            redundant_lines, merged_lines = processor.remove_redundant_lines()
 
-    processor.write(svg_output)
+        processor.write(svg_output)
+        logging.info('\n\n\nDone rendering to SVG: ' + svg_output)
 
-    logging.info('\n\n\nDone rendering to SVG: ' + svg_output)
+    if args.double_sided:
+        output_directory = os.path.join(source_parts_dir, 'build', 'fonts', 'front')
+        extra_variables['side'] = 1
+        render()
+        output_directory = os.path.join(source_parts_dir, 'build', 'fonts', 'back')
+        extra_variables['side'] = 2
+        render()
+    else:
+        render()
