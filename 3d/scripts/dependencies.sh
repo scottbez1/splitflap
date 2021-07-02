fd93d5f82333ab9879b2491b831b5dadbf57439d
Scott Bezek <scottbez1@gmail.com>
2021-07-01 21:15:57 -0700

    Improve 3d laser artifact generation (#158)
    - Add 4x panelized output generation
    - Generate text files that describe the dimensions of each SVG file (for verifying SVG processors - e.g. Ponoko - are handling dimensions correctly)
    - Add Elecrow wood kerf preset and zipped PDF generation: no etching, black lines, labeled dimensions, no optimization
    
    Example output pdf: [elecrow.pdf](https://github.com/scottbez1/splitflap/files/6751491/elecrow.pdf)

--- 3d/scripts/dependencies.sh
+++ 3d/scripts/dependencies.sh
@@ -7,6 +7,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 set -v
 
 sudo apt-get update -qq
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y openscad inkscape imagemagick xvfb
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y openscad inkscape imagemagick xvfb texlive-extra-utils
 
 sudo pip install -r "$DIR/requirements.txt"
--- 3d/scripts/generate_2d.py
+++ 3d/scripts/generate_2d.py
@@ -21,6 +21,7 @@ import logging
 import os
 import subprocess
 import sys
+import zipfile
 
 from svg_processor import SvgProcessor
 from projection_renderer import Renderer
@@ -35,6 +36,7 @@ from util import rev_info
 KERF_PRESETS = {
     'ponoko-3mm-mdf': 0.18,
     'ponoko-3mm-acrylic': 0.1,
+    'elecrow-3mm-wood': 0.185,
 }
 
 if __name__ == '__main__':
@@ -50,12 +52,23 @@ if __name__ == '__main__':
 
     parser.add_argument('--render-raster', action='store_true', help='Render raster PNG from the output SVG (requires '
                                                                      'Inkscape)')
+    parser.add_argument('--calculate-dimensions', action='store_true', help='Calculate overall cut file dimensions ('
+                                                                            'requires Inkscape)')
     parser.add_argument('--thickness', type=float, help='Override panel thickness value')
     parser.add_argument('--no-etch', action='store_true', help='Do not render laser-etched features')
     parser.add_argument('--mirror', action='store_true', help='Mirror the assembly so the outside faces are facing up. '
                                                               'Note that this will remove all etched features.')
+    parser.add_argument('--render-elecrow', action='store_true', help='Render an additional zipped pdf with labeled '
+                                                                      'dimensions and only cut lines, for Elecrow. '
+                                                                      'Requires Inkscape and pdfjam. Implies '
+                                                                      '--no-etch, --calculate-dimensions, and '
+                                                                      '--skip-optimize')
 
     args = parser.parse_args()
+    if args.render_elecrow:
+        args.no_etch = True
+        args.calculate_dimensions = True
+        args.skip_optimize = True
 
     laser_parts_directory = os.path.join(source_parts_dir, 'build', 'laser_parts')
 
@@ -78,16 +91,82 @@ if __name__ == '__main__':
     renderer.clean()
     svg_output = renderer.render_svgs(panelize_quantity=args.panelize)
 
-    logging.info('Removing redundant lines')
     processor = SvgProcessor(svg_output)
 
     redundant_lines, merged_lines = None, None
     if not args.skip_optimize:
+        logging.info('Removing redundant lines')
         redundant_lines, merged_lines = processor.remove_redundant_lines()
 
     processor.write(svg_output)
+    logging.info(f'\n\n\nDone rendering to SVG: {svg_output}')
 
-    logging.info('\n\n\nDone rendering to SVG: ' + svg_output)
+    if args.calculate_dimensions:
+        svg_for_dimensions = os.path.join(laser_parts_directory, os.path.splitext(os.path.basename(svg_output))[0] + '_dimensions.svg')
+        processor.apply_dimension_calculation_style()
+        processor.write(svg_for_dimensions)
+        logging.info('Read dimensions')
+        width_px = float(subprocess.check_output([
+            'inkscape',
+            '--without-gui',
+            '--query-width',
+            svg_for_dimensions,
+        ]))
+        width_mm = width_px * 25.4 / 96 # Assuming 96dpi...
+        height_px = float(subprocess.check_output([
+            'inkscape',
+            '--without-gui',
+            '--query-height',
+            svg_for_dimensions,
+        ]))
+        height_mm = height_px * 25.4 / 96 # Assuming 96dpi...
+        dimensions_file = os.path.join(laser_parts_directory, os.path.splitext(os.path.basename(svg_output))[0] + '_dimensions.txt')
+        with open(dimensions_file, 'w') as f:
+            f.write(f'{width_mm:.2f}mm x {height_mm:.2f}mm')
+
+    if args.render_elecrow:
+        elecrow_svg = os.path.join(laser_parts_directory, 'elecrow.svg')
+        elecrow_intermediate_pdf = os.path.join(laser_parts_directory, 'elecrow_intermediate.pdf')
+        elecrow_pdf = os.path.join(laser_parts_directory, 'elecrow.pdf')
+        elecrow_zip = os.path.join(laser_parts_directory, 'elecrow.zip')
+
+        processor.apply_elecrow_style()
+        processor.add_dimensions(width_mm, height_mm)
+        processor.write(elecrow_svg)
+
+        logging.info('Resize SVG canvas')
+        subprocess.check_call([
+            'inkscape',
+            '--verb=FitCanvasToDrawing',
+            '--verb=FileSave',
+            '--verb=FileClose',
+            '--verb=FileQuit',
+            elecrow_svg,
+        ])
+
+        logging.info('Output PDF')
+        subprocess.check_call([
+            'inkscape',
+            elecrow_svg,
+            '--export-pdf',
+            elecrow_intermediate_pdf,
+        ])
+
+        logging.info('Resize PDF')
+        subprocess.check_call([
+            'pdfjam',
+            '--letterpaper',
+            '--landscape',
+            '--noautoscale',
+            'true',
+            '--outfile',
+            elecrow_pdf,
+            elecrow_intermediate_pdf,
+        ])
+
+        logging.info('Zip')
+        with zipfile.ZipFile(elecrow_zip, 'w', zipfile.ZIP_DEFLATED) as zip:
+            zip.write(elecrow_pdf, 'elecrow.pdf')
 
     if args.render_raster:
         # Export to png
@@ -112,6 +191,7 @@ if __name__ == '__main__':
             '--verb=FileQuit',
             raster_svg,
         ])
+
         logging.info('Export PNG')
         subprocess.check_call([
             'inkscape',
