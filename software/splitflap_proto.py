af0da53dc5b9ed0e37ce5890ffcf0e302961b3fb
Scott Bezek <scottbez1@gmail.com>
2022-01-14 14:26:00 -0800

    WIP python proto library

--- software/splitflap_proto.py
+++ software/splitflap_proto.py
@@ -1,11 +1,21 @@
 from cobs import cobs
 from contextlib import contextmanager
+import logging
 import os
+from queue import (
+    Empty,
+    Full,
+    Queue,
+)
 import serial
 import serial.tools.list_ports
 import six
 import sys
-from threading import Thread
+from threading import (
+    Thread,
+    Lock,
+)
+import time
 import zlib
 
 software_root = os.path.dirname(os.path.abspath(__file__))
@@ -16,43 +26,129 @@ from proto_gen import splitflap_pb2
 
 class Splitflap(object):
 
-    def __init__(self, serial_instance):
-        self.serial = serial_instance
+    RETRY_TIMEOUT = 2 # TODO: set to 0.25
 
-    def _loop_for_status(self):
+    def __init__(self, serial_instance):
+        self._serial = serial_instance
+        self._logger = logging.getLogger('splitflap')
+        self._out_q = Queue()
+        self._ack_q = Queue()
+        self._next_nonce = 1
+        self._run = True
+
+        self._lock = Lock()
+        self._message_handler = None
+
+    def _read_loop(self):
+        self._logger.debug('Read loop started')
+        buffer = b''
         while True:
-            buffer = self.serial.read_until(b'\0')
+            buffer += self._serial.read_until(b'\0')
+            if not self._run:
+                return
+
             if not len(buffer):
                 continue
+
+            if not buffer.endswith(b'\0'):
+                continue
             
-            try:
-                decoded = cobs.decode(buffer[:-1])
-            except cobs.DecodeError:
-                print(f'Failed decode ({len(buffer)} bytes)')
-                print(buffer)
-                continue
+            self._process_frame(buffer[:-1])
+            buffer = b''
 
-            if len(decoded) < 4:
-                continue
+    def _process_frame(self, frame):
+        try:
+            decoded = cobs.decode(frame)
+        except cobs.DecodeError:
+            self._logger.debug(f'Failed decode ({len(frame)} bytes)')
+            self._logger.debug(frame)
+            return
 
-            payload = decoded[:-4]
-            expected_crc = zlib.crc32(payload) & 0xffffffff
-            provided_crc = (decoded[-1] << 24) \
-                            | (decoded[-2] << 16) \
-                            | (decoded[-3] << 8) \
-                            | decoded[-4]
-            
-            if expected_crc != provided_crc:
-                print(f'Bad CRC. expected={hex(expected_crc)}, actual={hex(provided_crc)}')
-                continue
+        if len(decoded) < 4:
+            return
 
-            message = splitflap_pb2.FromSplitflap()
-            message.ParseFromString(payload)
-            print(message)
+        payload = decoded[:-4]
+        expected_crc = zlib.crc32(payload) & 0xffffffff
+        provided_crc = (decoded[-1] << 24) \
+                        | (decoded[-2] << 16) \
+                        | (decoded[-3] << 8) \
+                        | decoded[-4]
+        
+        if expected_crc != provided_crc:
+            self._logger.debug(f'Bad CRC. expected={hex(expected_crc)}, actual={hex(provided_crc)}')
+            return
+
+        message = splitflap_pb2.FromSplitflap()
+        message.ParseFromString(payload)
+
+        # If this is an ack, notify the write thread
+        if message.WhichOneof("payload") == 'ack':
+            nonce = message.ack.nonce
+            self._ack_q.put(nonce)
+
+        self._logger.debug(message)
+        with self._lock:
+            if self._message_handler is not None:
+                try:
+                    self._message_handler(message)
+                except:
+                    self._logger.warning(f'Unhandled exception in message handler ({message.WhichOneof("payload")})', exec_info=True)
     
-    def setPositions(self, indexes):
+    def _write_loop(self):
+        self._logger.debug('Write loop started')
+        while True:
+            data = self._out_q.get()
+            # Check for shutdown
+            if not self._run:
+                self._logger.debug('Write loop exiting @ _out_q')
+                return
+            (nonce, encoded_message) = data
+
+            next_retry = 0
+            while True:
+                if time.time() >= next_retry:
+                    if next_retry > 0:
+                        self._logger.debug('Retry write...')
+                    self._serial.write(encoded_message)
+                    self._serial.write(b'\0')
+                    next_retry = time.time() + Splitflap.RETRY_TIMEOUT
+                
+                try:
+                    latest_ack_nonce = self._ack_q.get(timeout=next_retry - time.time())
+                except Empty:
+                    latest_ack_nonce = None
+
+                # Check for shutdown
+                if not self._run:
+                    self._logger.debug('Write loop exiting @ _ack_q')
+                    return
+
+                if latest_ack_nonce == nonce:
+                    break
+                else:
+                    self._logger.debug(f'Got unexpected nonce: {latest_ack_nonce}')
+    
+    def _enqueue_message(self, message):
+        nonce = self._next_nonce
+        self._next_nonce += 1
+
+        message.nonce = nonce
+
+        payload = bytearray(message.SerializeToString())
+
+        crc = zlib.crc32(payload) & 0xffffffff
+        payload.append(crc & 0xff)
+        payload.append((crc >> 8) & 0xff)
+        payload.append((crc >> 16) & 0xff)
+        payload.append((crc >> 24) & 0xff)
+
+        encoded_message = cobs.encode(payload)
+
+        self._out_q.put((nonce, encoded_message))
+        self._logger.debug(f'Out q length: {self._out_q.qsize()}')
+
+    def set_positions(self, indexes):
         message = splitflap_pb2.ToSplitflap()
-        # message.splitflap_command = splitflap_pb2.SplitflapCommand()
         for i in indexes:
             param = 0
             if i < 0:
@@ -64,27 +160,39 @@ class Splitflap(object):
             command.action = action
             command.param = param
             message.splitflap_command.modules.append(command)
-        
-        payload = bytearray(message.SerializeToString())
 
-        crc = zlib.crc32(payload) & 0xffffffff
-        payload.append(crc & 0xff)
-        payload.append((crc >> 8) & 0xff)
-        payload.append((crc >> 16) & 0xff)
-        payload.append((crc >> 24) & 0xff)
+        self._enqueue_message(message)
 
-        encoded = cobs.encode(payload)
-
-        self.serial.write(encoded)
-        self.serial.write(b'\0')
-
+    def start(self):
+        self.read_thread = Thread(target=self._read_loop)
+        self.write_thread = Thread(target=self._write_loop)
+        self.read_thread.start()
+        self.write_thread.start()
+    
+    def shutdown(self):
+        self._logger.debug('Shutting down')
+        self._run = False
+        self.read_thread.join()
+        self._logger.debug('Read thread terminated')
+        self._out_q.put(None)
+        self._ack_q.put(None)
+        self.write_thread.join()
+        self._logger.debug('Write thread terminated')
+    
+    def set_handler(self, message_handler):
+        with self._lock:
+            self._message_handler = message_handler
 
 
 @contextmanager
-def splitflap(serial_port):
+def splitflap_context(serial_port):
     with serial.Serial(serial_port, 230400, timeout=1.0) as ser:
         s = Splitflap(ser)
-        yield s
+        s.start()
+        try:
+            yield s
+        finally:
+            s.shutdown()
 
 
 def ask_for_serial_port():
@@ -106,10 +214,22 @@ def ask_for_serial_port():
 
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s:%(name)s:%(levelname)s:%(message)s')
+
     p = ask_for_serial_port()
-    with splitflap(p) as s:
-        t = Thread(target=s._loop_for_status)
-        t.start()
+    with splitflap_context(p) as s:
+        logging.info('Connected')
+        logging.info('Waiting for data...')
+        q = Queue(1)
+        def startup_handler(message):
+            try:
+                q.put_nowait(message)
+            except Full:
+                pass                
+        s.set_handler(startup_handler)
+        q.get()
+        s.set_handler(None)
+
         while True:
             input()
-            s.setPositions([1] * 108)
+            s.set_positions([1] * 108)
